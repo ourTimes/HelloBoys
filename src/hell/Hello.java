617e9b0c0eaef5c44552ad42729072b5a9164664
mr.hanxd <mr.hanxd@163.com>
2014-08-29 15:50:03 +0800

    测试提交

--- src/hell/Hello.java
+++ src/hell/Hello.java
@@ -3,7 +3,8 @@ package hell;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("This is our first main, let's go!");
 
 	}
 
